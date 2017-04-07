8092a41756c08ef878423362ae5a6e7d087a5448
Mubassir Hayat <mubassirhayat@gmail.com>
2017-04-07 16:44:53 +0500

    redundant comment

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,6 +1,5 @@
 <footer class="site-footer">
     <div class="container">
-        {{-- <h2>TechEase Solutions</h2> --}}
         <p>This Forum wouldn't be here without the help of out amazing community and ofcourse <a href="http://laravel.io">Laravel.io</a></p>
     </div>
 </footer>
