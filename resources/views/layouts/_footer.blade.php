8a7522d722422557d6edf1d2b4e54a2543aa3402
Mubassir Hayat <mubassirhayat@gmail.com>
2017-04-10 10:46:35 +0500

    spell

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="site-footer">
     <div class="container">
-        <p>This Forum wouldn't be here without the help of out amazing community and ofcourse <a href="http://laravel.io">Laravel.io</a></p>
+        <p>This Forum wouldn't be here without the help of our amazing community and ofcourse <a href="http://laravel.io">Laravel.io</a></p>
     </div>
 </footer>
